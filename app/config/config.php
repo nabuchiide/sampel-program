04eedff62e50cb172a6e5828c67a1ed7326f9608
Hendi Permana <59522515+nabuchiide@users.noreply.github.com>
2022-04-05 14:05:47 +0700

    initianComit

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('BASEURL','http://localhost/sampel/public');
+define('APL_NAME','sampel');
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME','sampel_program');
